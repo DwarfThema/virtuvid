02b1032bf66362ede0602c4d60a7a26edd1b677b
unknown <dwarfthema@gmail.com>
2023-11-17 18:19:51 +0900

    index_loading screen size

--- app/page.tsx
+++ app/page.tsx
@@ -32,16 +32,16 @@ export default function Home() {
 
   return (
     <main className="flex h-screen w-full flex-col items-center ">
-      {unityWebGL.isLoaded ? null : (
-        <div className="absolute w-full h-full z-30 flex flex-col items-center justify-center bg-stone-900 text-white">
-          <p>Now Loading...</p>
-          <p>{Math.round(unityWebGL.loadingProgression * 100)}%</p>
-        </div>
-      )}
       <div
         id="canvas-wrap"
         className="h-screen w-screen flex justify-center items-center"
       >
+        {unityWebGL.isLoaded ? null : (
+          <div className="absolute w-full h-full z-30 flex flex-col items-center justify-center bg-stone-900 text-white">
+            <p>Now Loading...</p>
+            <p>{Math.round(unityWebGL.loadingProgression * 100)}%</p>
+          </div>
+        )}
         <div id="avaturn-container" className="absolute h-[100%] w-[100%]">
           <iframe
             id="avaturn-iframe"
